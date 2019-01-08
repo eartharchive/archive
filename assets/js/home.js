251e08b68c7c768a3485869df426a03f95e8f9f8
Mubashir Aamir <m.mubashir.1994@gmail.com>
2019-01-08 13:05:20 +0500

    home page animation done

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,21 @@
-const homeContainer = document.querySelector("#home")
+import TweenLite from "gsap";
 
-if (homeContainer) {
-    console.log("home")
+var globeDrop = 250;
+
+function setGlobeDrop() {
+  if (window.innerWidth >= 768) {
+    globeDrop = 250;
+  } else {
+    globeDrop = 250;
+  }
 }
+
+function animateGlobe() {
+  TweenLite.to(".globe", 1, {
+    y: globeDrop, //490
+    rotation: 180
+  });
+}
+
+setGlobeDrop()
+animateGlobe();
