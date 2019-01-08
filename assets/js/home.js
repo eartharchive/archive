0da8cb7f0da20a9c57dea49500362e952be4b039
Mubashir Aamir <m.mubashir.1994@gmail.com>
2019-01-08 18:11:36 +0500

    all done

--- assets/js/home.js
+++ assets/js/home.js
@@ -19,3 +19,4 @@ function animateGlobe() {
 
 setGlobeDrop()
 animateGlobe();
+
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,26 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
+import TweenLite from 'gsap'
 
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.burger'), 0);
+var menuBtn = document.getElementById('menu-toggle-btn')
+
+menuBtn.addEventListener('click', toggleMenu, false)
+
+var btnScale = 0
+var btnRotation = 0
+var overlayScale = 0
+var menuVisible = false
+
+
+function toggleMenu() {
   
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        console.log("we have burger")
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-  
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-  
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
-  });
+  TweenLite.to('#close-menu-btn', .2, {
+    scale: Math.abs(btnScale - 1),
+    rotation: Math.abs(btnRotation - 180),
+    display: 'inline-flex'
+  })
+
+  TweenLite.to('#burger-menu-btn', .2, {
+    scale: btnScale,
+  })
+
+  btnScale = Math.abs(btnScale - 1)
+  btnRotation = Math.abs(btnRotation - 180)
+
+  overlayScale = Math.abs(overlayScale - 100 ) ;
+  TweenLite.to('.menu-overlay', .4, {
+    scale: overlayScale,
+  })
+
+  var menu = document.getElementById('menu')
+
+  if(!menuVisible){
+    menu.style.display = 'block'
+  }else{
+    menu.style.display = 'none'
+  }
+  menuVisible = !menuVisible
+}
