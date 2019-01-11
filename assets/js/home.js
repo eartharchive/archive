d493a9a08e893fde022024cc696aa55a314d5a34
Mubashir Aamir <m.mubashir.1994@gmail.com>
2019-01-11 16:37:45 +0500

    globe animation redone.

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,12 +4,43 @@ const landing = document.getElementById('landing');
 
 if (landing) {
   const globe = document.getElementById('globe');
+  const folder = document.getElementById('folder')
   window.addEventListener( 'scroll', function() {
     console.log(window.scrollY);
     // adjust size of element top when scroll more than ...
     //globe.style.width = 
-    globe.style.transform = "translate(0px, " + window.scrollY + "px);";
+    //globe.style.transform = "translate(0px, " + window.scrollY + "px);";
 
+    
+    var globeSize = (1000-window.scrollY) / 1000
+    var globeTranslate = 0
+    console.log(globeSize);
+    if(globeSize > 1){
+      globeSize = 1
+    }
+    if(globeSize < 0.7){
+      globeSize = 0.7
+    }
+
+    if(window.scrollY <= 300){
+      globeTranslate = window.scrollY
+    }else{
+      globeTranslate = 300
+    }
+
+    var translateVal =  "translateY(" + globeTranslate + "px)";
+
+    if(globeTranslate > 290){
+      folder.classList.add('opacityTransition')
+    }
+
+    
+
+    var scaleVal = " scale(" + globeSize + ")"
+
+    globe.style.transform = translateVal + scaleVal
+    globe.style.msTransform = translateVal + scaleVal
+    globe.style.webkitTransform = translateVal + scaleVal
   });
 
   let globeDrop = 250;
