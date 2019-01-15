9f620fee66fc144a325a1e5d967c7ddde29f005e
angel <angel.imaz@hush.com>
2019-01-16 00:44:59 +0100

    Update routes

--- lib/archive_web/router.ex
+++ lib/archive_web/router.ex
@@ -7,7 +7,7 @@ defmodule ArchiveWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug ArchiveWeb.AuthenticationController
+    #plug ArchiveWeb.AuthenticationController
   end
 
   pipeline :api do
